15915fa1b3ec5b98383673e0557550371a7905fc
madhavmanohars <madhav.ms@inapp.com>
2018-07-19 16:10:39 +0530

    master

---
+++ COUNT_WORDS.py
@@ -0,0 +1,6 @@
+file_handle1=open('/home/madhav/PycharmProjects/words.txt',"r")
+sent=file_handle1.read()
+ls=sent.split(' ')
+d={x.rstrip():ls.count(x) for x in ls}
+print d
+print "THE TOTAL NUMBER OF WORDS IN FILE ARE:"+" "+str(len(ls))
